6ab8f241cc6a15c5628731cf6505285770cf47c9
Nithin <nitmax.iaf@gmail.com>
2026-09-08 20:27:12 +0530

    notebook modification

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
 
-DATA_PATH = ROOT / "data" / "cars24-car-price-cleaned-new.csv"
+DATA_PATH = ROOT / "data" / "car_data.csv"
 MODEL_PATH = ROOT / "models" / "xgb_car_price_model.pkl"
 
 
